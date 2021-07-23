f007f96217a0a39852c6b75f0e2f675bed0e008e
Adam Woerz <azreow@gmail.com>
2021-07-23 17:56:09 -0400

    intial commit

--- TestingPushingToGithub/Program.cs
+++ TestingPushingToGithub/Program.cs
@@ -10,6 +10,8 @@ namespace TestingPushingToGithub
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
         }
     }
 }
